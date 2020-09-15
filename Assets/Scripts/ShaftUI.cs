20514951f1134f17b4a436499b856a11283cc076
Drinnn <pedro_lontro@hotmail.com>
2020-09-15 12:42:25 -0300

    feat: add shaft ui

---
+++ Assets/Scripts/ShaftUI.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class ShaftUI : MonoBehaviour {
+    [SerializeField] private TextMeshProUGUI currentGoldText;
+
+    private Shaft _shaft;
+
+    private void Awake() {
+        _shaft = GetComponent<Shaft>();
+    }
+
+    private void Update() {
+        currentGoldText.SetText(_shaft.CurrentDeposit.CurrentGold.ToString());
+    }
+}
