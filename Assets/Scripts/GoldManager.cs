32ebeb538d4612a8f63579ebf9acf8ffbb6849b2
Drinnn <pedro_lontro@hotmail.com>
2020-09-15 18:09:31 -0300

    feat: add gold manager with simple singleton

---
+++ Assets/Scripts/GoldManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class GoldManager : MonoBehaviour {
+    public static GoldManager Instance;
+
+    public int CurrentGold { get; set; }
+
+    [SerializeField] private int testGold = 0;
+
+    private readonly string GOLD_KEY = "GoldKey";
+
+    private void Awake() {
+        Instance = this;
+    }
+
+    private void Start() {
+        LoadGold();
+    }
+
+    private void LoadGold() {
+        CurrentGold = PlayerPrefs.GetInt(GOLD_KEY, testGold);
+    }
+
+    public void AddGold(int amount) {
+        CurrentGold += amount;
+        SaveGold();
+    }
+
+    public void RemoveGold(int amount) {
+        CurrentGold -= amount;
+        SaveGold();
+    }
+
+    private void SaveGold() {
+        PlayerPrefs.SetInt(GOLD_KEY, CurrentGold);
+        PlayerPrefs.Save();
+    }
+
+}
