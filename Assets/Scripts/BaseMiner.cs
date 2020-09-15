e5e408aa3aff412efc4dec3a0689d53922d8fe52
Drinnn <pedro_lontro@hotmail.com>
2020-09-14 22:28:18 -0300

    chore: add existing project

---
+++ Assets/Scripts/BaseMiner.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using UnityEngine;
+using DG.Tweening;
+
+public class BaseMiner : MonoBehaviour
+{
+    public int CurrentGold { get; set; }
+    public int CollectCapacity { get; set; }
+    public float CollectPerSecond { get; set; }
+    public bool IsTimeToCollect { get; set; }
+
+    private float _moveSpeed = 5f;
+    private int _initialCollectCapacity = 200;
+    private float _goldCollectPerSecond = 50f;
+
+    private void Awake()
+    {
+        CollectCapacity = _initialCollectCapacity;
+        CollectPerSecond = _goldCollectPerSecond;
+        IsTimeToCollect = true;
+    }
+
+    public virtual void Move(Vector3 newPosition)
+    {
+        transform.DOMove(newPosition, 10 / _moveSpeed).OnComplete((() =>
+        {
+            if (IsTimeToCollect)
+                CollectGold();
+            else
+                DepositGold();
+        })).Play();
+    }
+
+    public void ChangeGoal()
+    {
+        IsTimeToCollect = !IsTimeToCollect;
+    }
+
+    public void Rotate(int direction)
+    {
+        transform.localScale = new Vector3(direction, transform.localScale.y, transform.localScale.y);
+    }
+    protected virtual void CollectGold()
+    {
+
+    }
+
+    protected virtual void DepositGold()
+    {
+
+    }
+
+    protected virtual IEnumerator IECollect(int collectGold, float collectTime)
+    {
+        yield return null;
+    }
+}
---
+++ Assets/Scripts/ShaftMiner.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class ShaftMiner : BaseMiner
+{
+    [SerializeField] private Transform shaftMiningLocation;
+    [SerializeField] private Transform shaftDepositLocation;
+
+    private Animator _animator;
+    private int _miningAnimationParameter = Animator.StringToHash("Mining");
+    private int _walkingAnimationParameter = Animator.StringToHash("Walking");
+
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            Move(shaftMiningLocation.position);
+        }
+    }
+
+    public override void Move(Vector3 newPosition)
+    {
+        base.Move(newPosition);
+        _animator.SetTrigger(_walkingAnimationParameter);
+    }
+
+    protected override void CollectGold()
+    {
+        float collectTime = CollectCapacity / CollectPerSecond;
+
+        _animator.SetTrigger(_miningAnimationParameter);
+        StartCoroutine(IECollect(CollectCapacity, collectTime));
+    }
+
+    protected override void DepositGold()
+    {
+        CurrentGold = 0;
+        ChangeGoal();
+        Rotate(1);
+        Move(shaftMiningLocation.position);
+    }
+
+    protected override IEnumerator IECollect(int collectGold, float collectTime)
+    {
+        yield return new WaitForSeconds(collectTime);
+
+        CurrentGold = collectGold;
+        ChangeGoal();
+        Rotate(-1);
+        Move(shaftDepositLocation.position);
+    }
+}
