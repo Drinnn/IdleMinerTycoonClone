2f87547b669fa5b37a5f3bcd0393b9cee1fd5fe6
Drinnn <pedro_lontro@hotmail.com>
2020-09-15 09:48:17 -0300

    feat: add deposit class/behaviour

---
+++ Assets/Scripts/Deposit.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Deposit : MonoBehaviour {
+    public int CurrentGold { get; set; }
+
+    public void DepositGold(int amount) {
+        CurrentGold += amount;
+    }
+
+    public void RemoveGold(int amount) {
+        CurrentGold -= amount;
+    }
+}
