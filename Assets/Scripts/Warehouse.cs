a8f9018a96362741e2b6da081d7867dae6292503
Drinnn <pedro_lontro@hotmail.com>
2020-09-16 23:34:33 -0300

    feat: add wareouse initial setup

---
+++ Assets/Scripts/Warehouse.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Warehouse : MonoBehaviour {
+    [SerializeField] private Deposit elevatorDeposit;
+    [SerializeField] private Transform elevatorLocation;
+    [SerializeField] private Transform warehouseDepositLocation;
+
+}
